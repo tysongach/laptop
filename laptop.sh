387f433ccad5760e919186e61c02df4b1555b1dc
Tyson Gach <tyson@tysongach.com>
2024-04-12 09:22:38 -0500

    Remove old App Store apps

--- laptop.sh
+++ laptop.sh
@@ -62,23 +62,17 @@ cask "transmit"
 cask "visual-studio-code"
 cask "zoom"
 
-mas "Better", id: 1121192229
-mas "Contrast", id: 1254981365
 mas "Darkroom", id: 953286746
-mas "Deliveries", id: 290986013
 mas "DuckDuckGo Privacy Essentials", id: 1482920575
 mas "Gifox", id: 1461845568
 mas "Hush", id: 1544743900
 mas "Instapaper Save", id: 1481302432
 mas "Magnet", id: 441258766
-mas "Notability", id: 360593530
 mas "Numbers", id: 409203825
 mas "Pages", id: 409201541
 mas "Pixelmator Pro", id: 1289583905
 mas "Sim Daltonism", id: 693112260
-mas "Speedify", id: 999025824
 mas "Time Out", id: 402592703
-mas "Tweetbot", id: 1384080005
 mas "Vinegar", id: 1591303229
 mas "Xcode", id: 497799835
 EOF
